7d4a412be55ad04915d7264ed7cb1ec2637979a6
D4n1el0 <d.penas.varela@gmail.com>
2022-07-30 22:43:58 +0200

    hello
    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,8 @@
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
+
+    
     
     // Validate that variables exist
     if(toggle && nav){
